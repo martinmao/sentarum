66613eff51277a9b1df671e19e266265d609f30a
Martin Mao <martinmao@icloud.com>
2020-11-01 02:59:24 +0800

    changes

--- sentarum-trading/trading-domain/src/main/java/io/scleropages/sentarum/trading/order/model/OrderRefund.java
+++ sentarum-trading/trading-domain/src/main/java/io/scleropages/sentarum/trading/order/model/OrderRefund.java
@@ -15,8 +15,12 @@
  */
 package io.scleropages.sentarum.trading.order.model;
 
+import io.scleropages.sentarum.core.model.primitive.Amount;
+
+import java.util.Date;
+
 /**
- * 退款单
+ * 退款（退货）单
  *
  * @author <a href="mailto:martinmao@icloud.com">Martin Mao</a>
  */
@@ -35,4 +39,58 @@ public interface OrderRefund {
      * @return
      */
     Order order();
+
+
+    /**
+     * 申请退款时间
+     *
+     * @return
+     */
+    Date requestTime();
+
+
+    /**
+     * 申请退款描述
+     *
+     * @return
+     */
+    String requestContent();
+
+
+    /**
+     * 退款总金额= sum({@link RefundLine#refundFee()})
+     *
+     * @return
+     */
+    Amount totalRefundFee();
+
+    /**
+     * 退款状态：
+     * <pre>
+     * 买家已经申请退款，等待卖家同意
+     * 卖家拒绝退款
+     * 卖家已经同意退货，等待买家退货
+     * 买家已经退货，等待卖家确认收货
+     * 卖家未收到货,拒绝退款
+     * 退款关闭
+     * 退款成功
+     * </pre>
+     *
+     * @return
+     */
+    Integer refundState();
+
+    /**
+     * 退款类型: 1买家申请退款，2:商家主动退款， 3:自动退款(订单关闭退款，拼团未成团退款，返现退款，团购返现退款，拒单退款...)
+     *
+     * @return
+     */
+    Integer refundType();
+
+    /**
+     * 备注说明
+     *
+     * @return
+     */
+    String refundTypeRemark();
 }
